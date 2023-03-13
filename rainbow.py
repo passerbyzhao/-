5dbf3243a38017e48258ae080f0ee96bbfcf498c
Passerby Zhao <1326123952@qq.com>
2023-03-13 14:59:49 +0800

    Update rainbow.py

--- rainbow.py
+++ rainbow.py
@@ -2,26 +2,35 @@ from random import choice
 from time import sleep
 
 
-FOREGROUND = list(range(30, 38))
-BACKGROUND = list(range(40, 48))
-MODE = list(range(0, 9))
+FOREGROUND = [str(i) for i in range(30, 38)]
+BACKGROUND = [str(i) for i in range(40, 48)]
+MODE = [str(i) for i in range(0, 9)]
 RESET = '\033[0m'
 
-def infinite_rainbow(word):
+
+def infinite_rainbow(word, background=True):
     while 1:
-        foreclolor = str(choice(FOREGROUND))
-        backcolor = str(choice(BACKGROUND))
+        foreclolor = choice(FOREGROUND)
+        backcolor = choice(BACKGROUND)
         while foreclolor[-1] == backcolor[-1]:
-            backcolor = str(choice(BACKGROUND))
-        print('\r\033[1;' + foreclolor + ';' + backcolor + 'm' + str(word) + RESET, end='')
+            backcolor = choice(BACKGROUND)
+        if background:
+            print('\r\033[1;' + foreclolor + ';' + backcolor + 'm' + str(word) + RESET, end='')
+        else:
+            print('\r\033[1;' + foreclolor + 'm' + str(word) + RESET, end='')
         sleep(0.1)
 
-def rainbow(word):
-    foreclolor = str(choice(FOREGROUND))
-    backcolor = str(choice(BACKGROUND))
+
+def rainbow(word, background=True):
+    foreclolor = choice(FOREGROUND)
+    backcolor = choice(BACKGROUND)
     while foreclolor[-1] == backcolor[-1]:
-        backcolor = str(choice(BACKGROUND))
-    print('\r\033[1;' + foreclolor + ';' + backcolor + 'm' + str(word) + RESET, end='')
+        backcolor = choice(BACKGROUND)
+    if background:
+        print('\r\033[1;' + foreclolor + ';' + backcolor + 'm' + str(word) + RESET, end='')
+    else:
+        print('\r\033[1;' + foreclolor + 'm' + str(word) + RESET, end='')
+        
 
 if __name__ == '__main__':
     infinite_rainbow('RAINBOWrainbowRainbow')
